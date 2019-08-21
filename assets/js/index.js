43819a3b80b3ec549569c247a61dbe4d8962e085
user.name <email@email.com>
2019-08-20 21:02:44 -0300

    adds some animations

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,59 @@
-var menuMobile = document.querySelector(".menu-mobile")
+var menuMobile = document.querySelector(".menu-mobile");
 var toggleButton = document.querySelector(".navbar__toggle-button");
 
 toggleButton.addEventListener('click', function () {
     menuMobile.classList.toggle('menu-mobile--opened')
-})
+});
+
+
+function isElementInView(element) {
+    let viewTop = window.scrollY;
+    let viewBot = viewTop + window.innerHeight;
+
+    let elemTop = element.offsetTop;
+    let elemBot = elemTop + element.clientHeight;
+
+    // console.log("VT: " + viewTop + "\nVB:" + viewBot*0.72 + "\nET:" + elemTop + "\nEB:" + elemBot);
+    return ((elemTop <= viewBot*0.72) && (elemBot >= viewTop));
+}
+
+function isElementOutView(element) {
+    let viewTop = window.scrollY;
+    let viewBot = viewTop + window.innerHeight;
+
+    let elemTop = element.offsetTop;
+    let elemBot = elemTop + element.clientHeight;
+
+    // console.log("VT: " + viewTop + "\nVB:" + viewBot*0.72 + "\nET:" + elemTop + "\nEB:" + elemBot);
+    return ((elemTop <= viewBot*0.72) && (elemBot >= viewTop));
+}
+
+function removeFromArray(array, item) {
+    console.log(array);
+    array.remove(item);
+
+    // for(var i = 0; i < array.length; i++) {
+    //     if (array[i] === item) {
+    //         array.splice(i, 1);
+    //     }
+    // }
+
+    console.log(array);
+}
+
+
+window.onload = function (event) {
+    document.getElementById("secIntro").focus();
+}
+
+window.onscroll = function (event) {
+    let sections = document.getElementsByTagName("section");
+    for (let section of sections) {
+        if (isElementInView(section)) {
+            section.classList.add("animate-section");
+            setTimeout(removeFromArray, 600, section.classList, "animate-section");
+        } else {
+
+        }
+    }
+}
